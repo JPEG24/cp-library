aac8581b1dd58bc3c7cc77949be84d6f8f3b2831
JPEG24 <0115jpeg@gmail.com>
2026-09-05 15:55:56 +0900

    refactor: enhance error handling and output formatting in test scripts

--- lib/tools/interactive/test.sh
+++ lib/tools/interactive/test.sh
@@ -1,9 +1,17 @@
-trap 'rm -f interactive/pipe; echo; echo stopped; exit 130' INT
+#!/bin/bash
+
+BLUE='\033[34m'
+GREEN='\033[32m'
+RED='\033[31m'
+RESET='\033[0m'
+
+trap 'rm -f interactive/pipe; echo; echo -e "[${BLUE}INFO${RESET}] stopped"; exit 130' INT
 
 rm -f interactive/pipe
 mkfifo interactive/pipe
 
 cnt=0
+
 while true; do
   ((cnt++))
 
@@ -14,14 +22,43 @@ while true; do
 
   st=("${PIPESTATUS[@]}")
 
-  if [ ${st[0]} -ne 0 ] || [ ${st[2]} -ne 0 ]; then
-    echo "wrong $cnt"
-    echo "a.out  : ${st[0]}"
-    echo "judge  : ${st[2]}"
-    break
+  if [ ${st[0]} -ne 0 ]; then
+    echo
+    echo
+    echo -e "[${RED}FAILURE${RESET}] ${RED}WA${RESET}"
+    echo -e "[${BLUE}INFO${RESET}] case #${cnt}"
+
+    echo
+    echo "output:"
+    cat interactive/out.txt
+
+    echo
+    echo "judge:"
+    cat interactive/judge.txt
+
+    rm -f interactive/pipe
+    exit 1
   fi
 
-  echo "correct $cnt"
+  if [ ${st[2]} -ne 0 ]; then
+    echo
+    echo
+    echo -e "[${RED}FAILURE${RESET}] judge.cpp ${RED}RE${RESET}"
+    echo -e "[${BLUE}INFO${RESET}] case #${cnt}"
+
+    echo
+    echo "output:"
+    cat interactive/out.txt
+
+    echo
+    echo "judge:"
+    cat interactive/judge.txt
+
+    rm -f interactive/pipe
+    exit 1
+  fi
+
+  printf "\r[${GREEN}SUCCESS${RESET}] %d AC" "$cnt"
 done
 
 rm -f interactive/pipe
--- lib/tools/random_test/test.sh
+++ lib/tools/random_test/test.sh
@@ -1,16 +1,125 @@
-trap 'echo; echo "stopped"; exit 130' INT
+#!/bin/bash
+
+BLUE='\033[34m'
+GREEN='\033[32m'
+RED='\033[31m'
+RESET='\033[0m'
+
+trap 'echo; echo -e "[${BLUE}INFO${RESET}] stopped"; exit 130' INT
 
 cnt=0
+
 while true; do
   ((cnt++))
-  ./random_test/gen > random_test/in.txt
-  timeout 2s ./a.out < random_test/in.txt > random_test/out1.txt
-  timeout 2s ./random_test/naive < random_test/in.txt > random_test/out2.txt
 
-  if ! diff random_test/out1.txt random_test/out2.txt; then
-    echo "found ${cnt}"
-    break
-  else
-    echo "match ${cnt}"
+  ./random_test/gen > random_test/in.txt || exit 1
+
+  timeout 2s ./a.out \
+    < random_test/in.txt \
+    > random_test/out1.txt
+  status1=$?
+
+  timeout 2s ./random_test/naive \
+    < random_test/in.txt \
+    > random_test/out2.txt
+  status2=$?
+
+  if [ $status1 -eq 124 ]; then
+    echo
+    echo
+    echo -e "[${RED}FAILURE${RESET}] ${RED}TLE${RESET}"
+    echo -e "[${BLUE}INFO${RESET}] case #${cnt}"
+
+    echo
+    echo "input:"
+    cat random_test/in.txt
+
+    echo
+    echo "output:"
+    cat random_test/out1.txt
+
+    exit 1
   fi
+
+  if [ $status2 -eq 124 ]; then
+    echo
+    echo
+    echo -e "[${RED}FAILURE${RESET}] naive.cpp ${RED}TLE${RESET}"
+    echo -e "[${BLUE}INFO${RESET}] case #${cnt}"
+
+    echo
+    echo "input:"
+    cat random_test/in.txt
+
+    echo
+    echo "expected:"
+    cat random_test/out2.txt
+
+    exit 1
+  fi
+
+  if [ $status1 -ne 0 ]; then
+    echo
+    echo
+    echo -e "[${RED}FAILURE${RESET}] ${RED}RE${RESET}"
+    echo -e "[${BLUE}INFO${RESET}] case #${cnt}"
+
+    echo
+    echo "input:"
+    cat random_test/in.txt
+
+    echo
+    echo "output:"
+    cat random_test/out1.txt
+
+    exit 1
+  fi
+
+  if [ $status2 -ne 0 ]; then
+    echo
+    echo
+    echo -e "[${RED}FAILURE${RESET}] naive.cpp ${RED}RE${RESET}"
+    echo -e "[${BLUE}INFO${RESET}] case #${cnt}"
+
+    echo
+    echo "input:"
+    cat random_test/in.txt
+
+    echo
+    echo "expected:"
+    cat random_test/out2.txt
+
+    exit 1
+  fi
+
+  if diff -q \
+    random_test/out1.txt \
+    random_test/out2.txt \
+    >/dev/null
+  then
+    printf "\r[${GREEN}SUCCESS${RESET}] %d AC" "$cnt"
+    continue
+  fi
+
+  echo
+  echo
+  echo -e "[${RED}FAILURE${RESET}] ${RED}WA${RESET}"
+  echo -e "[${BLUE}INFO${RESET}] case #${cnt}"
+
+  echo
+  echo "input:"
+  cat random_test/in.txt
+
+  echo
+  echo "output:"
+  cat random_test/out1.txt
+
+  echo
+  echo "expected:"
+  cat random_test/out2.txt
+
+  echo
+  echo -e "[${RED}FAILURE${RESET}] test ${RED}failed${RESET} after $((cnt - 1)) AC"
+
+  exit 1
 done
