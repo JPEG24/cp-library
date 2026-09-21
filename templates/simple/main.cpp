c9bba6bb2c5782f195a96fe90775faa11a6e1b42
JPEG24 <0115jpeg@gmail.com>
2026-09-21 10:27:38 +0900

    feat: add useful template files for problem solving

--- templates/simple/main.cpp
+++ templates/simple/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
-#define rep(i,n) for (int i = 0; i < n; i++)
+#define rep(i,n) for (int i = 0; i < (n); i++)
 
 int main() {
 	cin.tie(nullptr)->sync_with_stdio(false);
